39f353cf5bc639541ebd2f0765767c959f7f96c7
João Pedro Nogueira <jpedronogueira.dev@gmail.com>
2022-10-05 17:30:42 -0400

    Configuração numérica do countdown

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -28,6 +28,7 @@ export function Home() {
 
   const [cycles, setCycles] = useState<Cycle[]>([])
   const [activeCycleId, setActiveCycleId] = useState <string|null>(null)
+  const [secondsPassed, setSecondsPassed] = useState(0)
 
   const { register, handleSubmit, watch, reset } = useForm<newCycleFormData>({
     resolver: zodResolver(newCycleFormValidationSchema),
@@ -36,11 +37,6 @@ export function Home() {
       minutesAmount: 0
     }
   })
-  
-  // interface newCycleFormData {
-    //   task: string,
-    //   minutesAmount: number
-    // }
     
     type newCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
     
@@ -61,7 +57,12 @@ export function Home() {
 
     const activeCycle = cycles.find(cycle => cycle.id === activeCycleId)
 
-    console.log(activeCycle)
+    const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0
+    const currentSeconds = activeCycle ? totalSeconds - secondsPassed : 0
+    const minutesAmount = Math.floor(currentSeconds / 60)
+    const secondsAmount = currentSeconds % 60
+    const minutes = String(minutesAmount).padStart(2, '0')
+    const seconds = String(secondsAmount).padStart(2, '0')
     
     const task = watch('task')
     const isTaskEmpty = !task
@@ -100,11 +101,11 @@ export function Home() {
         </FormContainer>
 
         <CountdownContainer>
-          <span>0</span>
-          <span>0</span>
+          <span>{minutes[0]}</span>
+          <span>{minutes[1]}</span>
           <Separator>:</Separator>
-          <span>0</span>
-          <span>0</span>
+          <span>{seconds[0]}</span>
+          <span>{seconds[1]}</span>
         </CountdownContainer>
 
         <StarCountdownButton type="submit" disabled={isTaskEmpty}>
